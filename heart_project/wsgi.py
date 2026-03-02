4340e93a1583372a32b37fe23c8f19545791afc9
noomsky <noomsky123456@gmail.com>
2026-03-03 02:47:01 +0700

    Add auto-migrate to wsgi.py to fix missing table error

--- heart_project/wsgi.py
+++ heart_project/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for heart_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heart_project.settings')
+# 🌟 เพิ่มโค้ดสั่ง Migrate ตรงนี้
+os.system('python manage.py migrate')
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
 application = get_wsgi_application()
--- prediction/apps.py
+++ prediction/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
-
+import os
 
 class PredictionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'prediction'
+
+    def ready(self):
+        # 🌟 สั่งให้ทำงานเมื่อ App พร้อม
+        if os.environ.get('RUN_MAIN') == 'true': # ป้องกันการรันซ้ำซ้อนตอนเปิดเซิร์ฟเวอร์
+            os.system('python manage.py migrate')
